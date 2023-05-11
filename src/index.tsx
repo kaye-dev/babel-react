8135632c3753cecac850c13f545f86d3696cf524
kaye-dev <keien.nishikawa.developer@gmail.com>
2023-05-11 12:00:37 +0900

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Message } from "./ui/elements/message";
 import { TitleType, MessageType } from "./types";
+import { AppIcon } from "./ui/elements/AppIcon";
 
 type Props = {
   title: TitleType;
@@ -11,6 +12,7 @@ export function Hello(props: Props): React.JSX.Element {
   return (
     <div>
       <h1>{props.title}</h1>
+      <AppIcon width={24} />
       <Message message={props.message} />
     </div>
   );
